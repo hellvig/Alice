ffc01858d741683513b2f2e5f14020261b4ed7fa
hellvig <111068891+hellvig@users.noreply.github.com>
2026-09-14 17:08:36 -0300

    ll
    ll

--- index.php
+++ index.php
@@ -1,33 +1,51 @@
 <!DOCTYPE html>
-<html lang="PT-br">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I love pao de queijo</title>
-    <linl rel = "stylesheet" href="style.css">
+
+    <title>I love pão de queijo</title>
+
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-<div class="card">
+    <div class="card">
 
-    <?php
-echo "<h1>I love pao de queijo</h1>";
-$nome = "Alice";
-$idade = "18";
-?>
+        <?php
+            echo "<h1>I love pao de queijo</h1>";
 
-<h1> nome: <?= $nome ?> </h1>
-<p> idade <?= $idade ?> </p>
-    
-<p>
-        Status:
-        <?php if ($idade >=18): ?>
-            <strong style="color: green;">Maior de idade</strong>
-        <?php else: ?>
-            <strong style="color: red;">Menor de idade</strong>
-        <?php endif; ?>
-</p>
-</div>
+            $nome = "Alice";
+            $idade = 18;
+        ?>
+
+        <h1>Nome: <?= $nome ?></h1>
+
+        <p>Idade: <?= $idade ?></p>
+
+        <p>
+            Status:
+
+            <?php if ($idade >= 18): ?>
+
+                <strong style="color: green;">
+                    Maior de idade
+                </strong>
+
+            <?php else: ?>
+
+                <strong style="color: red;">
+                    Menor de idade
+                </strong>
+
+            <?php endif; ?>
+
+        </p>
+
+    </div>
 
 </body>
+
 </html>
